04867522b6258cba82a52f83f325cfa5949cea4e
Valentina <hujiayun@Valentinas-MacBook-Pro.local>
2022-09-20 19:21:27 +0800

    varaibles,print,conditionals

---
+++ index.py
@@ -0,0 +1,14 @@
+name="tina"
+print(name)
+
+x = 9
+y = 99
+print(x+y)
+
+isloggedin=False
+if isloggedin:
+    welcome_message="welcome"
+    print(welcome_message+" "+name+"!")
+
+else:
+    print("please log in")
