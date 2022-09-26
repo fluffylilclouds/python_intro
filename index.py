8a64f26fcca9135fe5c238676637a094196d3941
Valentina <hujiayun@Valentinas-MacBook-Pro.local>
2022-09-26 20:37:10 +0800

    conditions

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
-name="tina"
+name="tina" # this is a string variable
 print(name)
 
-x = 9
+x = 5
 y = 99
 print(x+y)
 
@@ -12,3 +12,35 @@ if isloggedin:
 
 else:
     print("please log in")
+
+if name=="tina":
+    print("welcome tina")
+elif name=="tommy":
+    print("welcome tommy")
+else:
+    print("welcome aliens")
+
+
+if x<=5:
+    print("abc")
+elif 5<x:
+    print("xyz")
+
+
+if y>99 and name=="tina": # and (2 conditions are true)
+    print("i will not print")
+
+
+if y>99 or  name=="tina": # or (1 condition is true)
+    print("i will print")
+
+
+if y<100: # first condition must be true to come into the next block 'name check'
+    if name=="tina":
+        print("I will also print")
+    else:
+        print("...")
+else:
+    print("I will not print")
+
+
